b61b24e8f85e114ee099e65564e8b1f50448d99e
unknown <juanrivas19@iespuntadelverde.es>
2020-09-29 10:08:05 +0200

    mi primer commit

---
+++ src/tiposbasicos.ts
@@ -0,0 +1,16 @@
+/*
+ver:
+Documentación typescript
+*/
+let activo: boolean
+let valor: number
+let cantidad: ( string | number)[]
+let nombre: string
+let lista: number[]
+let edad = 30
+activo = false
+if (!activo) {
+    console.log('No está activo')
+} else {
+    console.log('Está activo')
+}
